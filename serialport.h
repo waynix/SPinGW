c13ccdb8beee1f5564110143b013dca4b6513fd8
Paul Rosset <paul@r0sset.com>
2012-03-26 21:34:30 +0200

    emums added

--- serialport.h
+++ serialport.h
@@ -2,8 +2,84 @@
 #define SERIALPORT_H
 #include <inttypes.h>
 #include <windows.h>
+//#include <strsafe.h>
+enum Baudrate
+{
+	B50		= 50,
+	B110	= 110,
+	B150	= 150,
+	B300	= 300,
+	B1200	= 1200,
+	B2400	= 2400,
+	B4800	= 4800,
+	B9600   = 9600,
+	B19200	= 19200,
+	B38400	= 38400,
+	B57600	= 57600,
+	B115200 = 115200,
+	B230400	= 230400,
+	B460800	= 460800,
+	B500000 = 500000,
+	B1000000= 1000000
+};
+enum Stopbits
+{
+	one = ONESTOPBIT,
+	onePointFive= ONE5STOPBITS,
+	two = TWOSTOPBITS
+};
+enum Paritycheck
+{
+	even = EVENPARITY,
+	odd  = ODDPARITY,
+	off  = NOPARITY,
+	mark = MARKPARITY	
+};
 
-HANDLE openSerialPort(LPCSTR portname)
+
+void ErrorExit(LPTSTR lpszFunction) 
+{ 
+    // Retrieve the system error message for the last-error code
+
+    LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+    DWORD dw = GetLastError(); 
+
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR) &lpMsgBuf,
+        0, NULL );
+
+    // Display the error message and exit the process
+
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, 
+        (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR)); 
+    sprintf((LPTSTR)lpDisplayBuf, 
+        TEXT("%s failed with error %d: %s"), 
+        lpszFunction, dw, lpMsgBuf); 
+    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK); 
+
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
+    ExitProcess(dw); 
+}
+
+
+
+/**
+	\brief Opens a new connection to a serial port
+	\param portname		name of the serial port(COM1 - COM9 or \\\\.\\COM1-COM256)
+	\param baudrate		the baudrate of this port (for example 9600)
+	\param stopbits		th nuber of stoppbits (one, onePointFive or two)
+	\param parity		the parity (even, odd, off or mark)
+	\return				HANDLE to the serial port
+	*/
+HANDLE openSerialPort(LPCSTR portname,enum Baudrate baudrate, enum Stopbits stopbits, enum Paritycheck parity)
 {
 	DWORD  accessdirection =GENERIC_READ | GENERIC_WRITE;
 	HANDLE hSerial = CreateFile(portname,
@@ -14,19 +90,19 @@ HANDLE openSerialPort(LPCSTR portname)
 		0,
 		0);
 	if (hSerial == INVALID_HANDLE_VALUE) {
-		//call GetLastError(); to gain more information
+		ErrorExit("Create File failed");
 	}
 	DCB dcbSerialParams = {0};
 	dcbSerialParams.DCBlength=sizeof(dcbSerialParams);
 	if (!GetCommState(hSerial, &dcbSerialParams)) {
-		 //could not get the state of the comport
+		 ErrorExit("could not retrieve state");
 	}
-	dcbSerialParams.BaudRate=460800;
+	dcbSerialParams.BaudRate=baudrate;
 	dcbSerialParams.ByteSize=8;
-	dcbSerialParams.StopBits=ONESTOPBIT;
-	dcbSerialParams.Parity=NOPARITY;
+	dcbSerialParams.StopBits=stopbits;
+	dcbSerialParams.Parity=parity;
 	if(!SetCommState(hSerial, &dcbSerialParams)){
-		 //analyse error
+		 ErrorExit("could not set State");
 	}
 	COMMTIMEOUTS timeouts={0};
 	timeouts.ReadIntervalTimeout=50;
@@ -35,27 +111,40 @@ HANDLE openSerialPort(LPCSTR portname)
 	timeouts.WriteTotalTimeoutConstant=50;
 	timeouts.WriteTotalTimeoutMultiplier=10;
 	if(!SetCommTimeouts(hSerial, &timeouts)){
-		//handle error
+		ErrorExit("could not set timeouts");
 	}
 	return hSerial;
 }
 
-
+/**
+	\brief Read data from the serial port
+	\param hSerial	File HANDLE to the serial port
+	\param buffer	pointer to the area where the read data will be written
+	\param buffersize maximal size of the buffer area
+	\return			amount of data that was read
+	*/
 DWORD readFromSerialPort(HANDLE hSerial, uint8_t * buffer, int buffersize)
 {
     DWORD dwBytesRead = 0;
     if(!ReadFile(hSerial, buffer, buffersize, &dwBytesRead, NULL)){
-        //handle error
+        ErrorExit("reading");
     }
     return dwBytesRead;
 }
 
+/**
+	\brief write data to the serial port
+	\param hSerial	File HANDLE to the serial port
+	\param buffer	pointer to the area where the read data will be read
+	\param length	amount of data to be read
+	\return			amount of data that was written
+	*/
 DWORD writeToSerialPort(HANDLE hSerial, uint8_t * data, int length)
 {
 
 	DWORD dwBytesRead = 0;
 	if(!WriteFile(hSerial, data, length, &dwBytesRead, NULL)){
-		//handle error
+		ErrorExit("writing");
 	}
 	return dwBytesRead;
 
