6b98cade07070efdd427e702daa6a9bef73ce9d0
waynix <github@klappezu.org>
2012-03-26 23:23:41 +0300

    layout

--- serialport.h
+++ serialport.h
@@ -2,10 +2,9 @@
 #define SERIALPORT_H
 #include <inttypes.h>
 #include <windows.h>
-//#include <strsafe.h>
 enum Baudrate
 {
-	B50		= 50,
+	B50	= 50,
 	B110	= 110,
 	B150	= 150,
 	B300	= 300,
@@ -25,7 +24,7 @@ enum Baudrate
 enum Stopbits
 {
 	one = ONESTOPBIT,
-	onePointFive= ONE5STOPBITS,
+	onePointFive = ONE5STOPBITS,
 	two = TWOSTOPBITS
 };
 enum Paritycheck
@@ -77,7 +76,7 @@ void ErrorExit(LPTSTR lpszFunction)
 	\param baudrate		the baudrate of this port (for example 9600)
 	\param stopbits		th nuber of stoppbits (one, onePointFive or two)
 	\param parity		the parity (even, odd, off or mark)
-	\return				HANDLE to the serial port
+	\return			HANDLE to the serial port
 	*/
 HANDLE openSerialPort(LPCSTR portname,enum Baudrate baudrate, enum Stopbits stopbits, enum Paritycheck parity)
 {
@@ -118,8 +117,8 @@ HANDLE openSerialPort(LPCSTR portname,enum Baudrate baudrate, enum Stopbits stop
 
 /**
 	\brief Read data from the serial port
-	\param hSerial	File HANDLE to the serial port
-	\param buffer	pointer to the area where the read data will be written
+	\param hSerial	  File HANDLE to the serial port
+	\param buffer	  pointer to the area where the read data will be written
 	\param buffersize maximal size of the buffer area
 	\return			amount of data that was read
 	*/
@@ -141,13 +140,11 @@ DWORD readFromSerialPort(HANDLE hSerial, uint8_t * buffer, int buffersize)
 	*/
 DWORD writeToSerialPort(HANDLE hSerial, uint8_t * data, int length)
 {
-
 	DWORD dwBytesRead = 0;
 	if(!WriteFile(hSerial, data, length, &dwBytesRead, NULL)){
 		ErrorExit("writing");
 	}
 	return dwBytesRead;
-
 }
 
 void closeSerialPort(HANDLE hSerial)
